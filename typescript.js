6e81ff6d22ce92c25566887ecdfeb9978a90f747
Front-End-Lessonss <150601672+Front-End-Lessonss@users.noreply.github.com>
2024-05-11 16:21:52 +0400

    Add files via upload

---
+++ typescript.js
@@ -0,0 +1,12 @@
+// let a : string;
+// a = 'Lale'
+// console.log(a);
+// let students : string[];
+// let numbers : number[];
+// let answers : boolean[];
+// let name : Array<string>;
+// students = ['lale', 'nana'];
+// numbers = [1, 2, 3];
+// answers = [true , false];
+// name = ['aysel'];
+// console.log(students, numbers, answers, name)
